dea8b2a288a893631c4161e0fc1e1dd0dfa696e8
jordles113 <jordan.t.romero@gmail.com>
2021-02-24 15:26:13 -0600

    completes variables lesson

--- src/Hello.java
+++ src/Hello.java
@@ -3,8 +3,8 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, Jordan");
 
-        int myFirstNumber = 5; 
-        
+        int myFirstNumber = (10 + 5) + (2 * 10); 
+
         System.out.println(myFirstNumber);
     }
 }
