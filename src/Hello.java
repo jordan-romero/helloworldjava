4680fbda79f52b14c566bd9fffc287a80c4aeec4
jordles113 <jordan.t.romero@gmail.com>
2021-02-24 15:32:21 -0600

    completes the challenge

--- src/Hello.java
+++ src/Hello.java
@@ -4,7 +4,10 @@ public class Hello {
         System.out.println("Hello, Jordan");
 
         int myFirstNumber = (10 + 5) + (2 * 10); 
-
-        System.out.println(myFirstNumber);
+        int mySecondNumber = 12; 
+        int myThirdNumber = myFirstNumber * 2;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber; 
+        int myLastOne = myTotal - 1000; 
+        System.out.println(myLastOne);
     }
 }
