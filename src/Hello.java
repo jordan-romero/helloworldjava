452816e3b14708d1ea3d0f879006947294948afe
jordles113 <jordan.t.romero@gmail.com>
2021-02-24 15:17:48 -0600

    completes small challenge

--- src/Hello.java
+++ src/Hello.java
@@ -2,5 +2,9 @@ public class Hello {
 
     public static void main(String[] args) {
         System.out.println("Hello, Jordan");
+
+        int myFirstNumber = 5; 
+        
+        System.out.println(myFirstNumber);
     }
 }
